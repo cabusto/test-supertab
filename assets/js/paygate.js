1e8c62fffaa920ee8c78a771c3dbb4543adbbd60
Justin Warren <justinkwarren@gmail.com>
2025-05-01 13:02:54 -0230

    Update paygate.js to new May 1

--- assets/js/paygate.js
+++ assets/js/paygate.js
@@ -1,39 +1,33 @@
-// assets/js/paygate.js
 
-    import { createPaygateExperience } from "https://js.sbx.supertab.co/v2/experiences.js";
+      <script type="module">
+        import { createPaygateExperience } from "https://js.sbx.supertab.co/v2/experiences.js";
 
-    const { show } = await createPaygateExperience({
-      clientId: "test_client.22c8c287-577b-473e-b18e-60ae1f560401",
-      experienceId: "experience.9d0e463d-4f6a-4b12-9f28-054020486019",
-      onPurchaseCompleted: () => {
-        //insert your code to grant user access
-        console.log("Paygate completed!");
-      },
-      onPurchaseCanceled: () => {
-        //insert your code to handle when purchase is not completed
-        console.log("Paygate canceled!");
+        const { show } = await createPaygateExperience({
+          clientId: "test_client.1482aaab-3734-4e12-b6dd-38daf89e3123",
+          experienceId: "experience.6a205014-a962-4a35-9d9d-1b45e959ba23",
+          onPurchaseCompleted: () => {
+            //insert your code to grant user access
+            console.log("Paygate completed!");
+          },
+          onPurchaseCanceled: () => {
+            //insert your code to handle when purchase is not completed
+            console.log("Paygate canceled!");
 
-        // Redirect to the paygateRedirectUrl
-        window.location.href = "https://cabusto.github.io";
-      },
-      onPriorEntitlement: () => {
-        //insert your code to handle when user has prior entitlement
-        console.log("Paygate: User has prior entitlement. Showing the content.");
-      },
-      onError: (error) => {
-        //insert your code to handle an unexpected error
-        console.error("Paygate error!", error);
-      },
-    })
+            // Redirect to the paygateRedirectUrl
+            window.location.href = "https://cabusto.github.io";
+          },
+          onPriorEntitlement: () => {
+            //insert your code to handle when user has prior entitlement
+            console.log("Paygate: User has prior entitlement. Showing the content.");
+          },
+          onError: (error) => {
+            //insert your code to handle an unexpected error
+            console.error("Paygate error!", error);
+          },
+        })
 
-    // The "show()" method call is an example of how to trigger the Paygate.
-    // For custom behavior, use methods like "hide", "toggle", "isShowing", or "destroy".
-    show();
-
+        // The "show()" method call is an example of how to trigger the Paygate.
+        // For custom behavior, use methods like "hide", "toggle", "isShowing", or "destroy".
+        show();
+      </script>
     
-
-
-
-
-
-
